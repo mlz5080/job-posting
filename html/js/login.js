6b19a6749679416dc7390da84acbd56224e1bccb
Ling <mlz961116@gmail.com>
2019-02-19 20:11:10 -0500

    Add firebase state persistence

--- html/js/login.js
+++ html/js/login.js
@@ -30,6 +30,21 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+  .then(function() {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
 function Signup()
 {
 
