e7761488f77a91a377ffc8f0fb4589a23efeb098
Andy <unispace@Andys-MacBook-Pro.local>
2019-02-19 21:58:09 -0500

    html

--- html/js/login.js
+++ html/js/login.js
@@ -74,3 +74,20 @@ function Logout(){
 
 
 }
+
+function phoneLogin(){
+
+  var phoneNumber = document.getElementById('phone').value;
+var appVerifier = window.recaptchaVerifier;
+firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+    .then(function (confirmationResult) {
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      window.confirmationResult = confirmationResult;
+    }).catch(function (error) {
+      // Error; SMS not sent
+      // ...
+    });
+
+
+}
