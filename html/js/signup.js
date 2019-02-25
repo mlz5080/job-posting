64bca24bdae6106a2f7bd5f0541082200ec9c5fd
Ling <mlz961116@gmail.com>
2019-02-24 22:19:14 -0500

    fix on login.html

--- html/js/signup.js
+++ html/js/signup.js
@@ -1,4 +1,4 @@
-  firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     console.log("Still login!")
     window.location.href="education.html";
@@ -23,25 +23,41 @@ firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
     var errorMessage = error.message;
   });
 
-  function Signup(){
+function Signup(){
 
-  	var email = document.getElementById('email').value;
-  	var password = document.getElementById('password').value;
-    	var passwordc = document.getElementById('passwordc').value;
+	var email = document.getElementById('email').value;
+	var password = document.getElementById('password').value;
+  	var passwordc = document.getElementById('passwordc').value;
 
-      if(email == '' && password == ''){
-        window.alert("信息不能为空")
-        window.location.href="signup.html";
-      }
-    else if (password == passwordc){
-    window.alert("Successful!")
-  	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  	// Handle Errors here.
-  	var errorCode = error.code;
-  	var errorMessage = error.message;
-  	window.alert("Error: " + errorMessage);
-    });
-    }else{
-    window.alert("两次密码输入不一致，请再次确认");
+    if(email == '' && password == ''){
+      window.alert("信息不能为空")
+      window.location.href="signup.html";
     }
+  else if (password == passwordc){
+  window.alert("Successful!")
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+	// Handle Errors here.
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	window.alert("Error: " + errorMessage);
+  });
+  }else{
+  window.alert("两次密码输入不一致，请再次确认");
   }
+}
+
+function Login(){
+
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function (){
+
+    window.location.href='index.html';
+  }).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  window.alert("Error: " + errorMessage);
+
+  });
+}
