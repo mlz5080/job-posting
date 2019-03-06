8f1d2898672f3f6509c27a54798a75177c0a4ad9
Ling <mlz961116@gmail.com>
2019-03-06 11:17:36 -0500

    fixed job posting timestamp missing

--- html/js/jobUpdate.js
+++ html/js/jobUpdate.js
@@ -96,7 +96,7 @@ function writeUserData(userId,companyname, email, location,jobtitle, jobtype,job
         companyweb:companyweb,
         requirement: requirement,
         description:description,
-
+        timestamp:Math.floor(Date.now() / 1000);
     };
     var updates={};
     updates['/posts/' + userId ] = postData;
